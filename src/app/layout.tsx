ff7e0c8d2514e391fdd814565e37365239f6698e
Aikhan Jumashukurov <ajumashukurov@gmail.com>
2025-07-26 07:01:57 +0600

    Modified main tab

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,17 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import MainLayout from "./MainLayout";
 import ClientProviders from "./ClientProviders";
+import { Metadata } from "next";
 
 const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: 'Devmolink',
+  description: 'Connect with other developers. Whether for a job, a project, a relationship, devmolink is the place to be. Stop calling us the Tinder for developers.',
+  icons: {
+    icon: '/devmolinkGreenLogo.svg'
+  }
+};
 
 export default function RootLayout({
   children,
